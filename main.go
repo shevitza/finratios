095375f4a1418528f5492aa9637a3585d20b8123
Evgenia Hristova <shevitza@gmail.com>
2021-10-15 15:37:56 +0300

    Update main.go
    A function init for database connection string added.

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"os"
 	"log"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 )
-var (
-    outfile, _ = os.Create("finratios.log") // log file path
-    l      = log.New(outfile, "", 0)
-)
-
 
 func main() {
+
+
 	start := time.Now()
 
 	stepByStep("first_step")
@@ -38,4 +36,32 @@ func stepByStep(step string) {
 	calcall_func1(indicators, datecompany, numrows)
 	calc_adj(indicators, datecompany, numrows)
 	calc_sumsub(indicators, datecompany)
+}
+
+func logerror(logfile string, errorstring string) {
+
+	// If the file doesn't exist, create it, or append to the file
+	f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := f.Write([]byte(errorstring)); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func init() {
+	fmt.Println(time.Now().Format("2006.01.02 15:04:05"))
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	conn = os.Getenv("DBUSER") + ":" + os.Getenv("DBPASS") + "@tcp(" + os.Getenv("HOST") + ":" + os.Getenv("PORT") + ")/" + os.Getenv("DBNAME")
+
 }
